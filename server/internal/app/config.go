668a04e5e931481e3a48c000e7d3f1346341ebc5
ainurbl <ainur19dota2@mail.ru>
2022-09-04 19:39:37 +0300

    add storage interface, use regular map instead of db

--- server/internal/app/config.go
+++ server/internal/app/config.go
@@ -1,14 +1,18 @@
 package server
 
+import "github.com/sava-cska/SPbSU-Calculator/internal/app/storage"
+
 type Config struct {
 	BindAddress string `toml:"bind_address"`
 	LogLevel    string `toml:"log_level"`
+	Storage     *storage.Config
 }
 
-// NewConfig Create new instance of config
+// NewConfig Upsert new instance of config
 func NewConfig() *Config {
 	return &Config{
 		BindAddress: ":8080",
 		LogLevel:    "debug",
+		Storage:     storage.NewConfig(),
 	}
 }
---
+++ server/internal/app/storage/storage.go
@@ -0,0 +1,50 @@
+package storage
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Storage struct {
+	config         *Config
+	db             *sql.DB
+	evaluationsDao *EvaluationsDAO
+}
+
+func New(config *Config) *Storage {
+	return &Storage{
+		config: config,
+	}
+}
+
+func (storage *Storage) Open() error {
+	db, err := sql.Open("postgres", storage.config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	storage.db = db
+
+	return nil
+}
+
+func (storage *Storage) Close() {
+	storage.db.Close()
+}
+
+func (storage *Storage) Evaluations() *EvaluationsDAO {
+	if storage.evaluationsDao != nil {
+		return storage.evaluationsDao
+	}
+
+	storage.evaluationsDao = &EvaluationsDAO{
+		storage: storage,
+	}
+
+	return storage.evaluationsDao
+}
