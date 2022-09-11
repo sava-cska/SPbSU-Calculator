6e5f82cfaea6e8a4e0c515b5969b29670403f380
Saveliy Grigoryev <54453219+sava-cska@users.noreply.github.com>
2022-09-11 12:21:19 +0300

    Add handlers of get/post queries (#8)

--- server/internal/app/model/evaluation.go
+++ server/internal/app/model/evaluation.go
@@ -1,6 +1,22 @@
 package model
 
+import "strings"
+
+type Token struct {
+	Body string
+	Type string
+}
+
 type Evaluation struct {
-	UserId            string
-	EncodedEvaluation string
+	UserUid    string
+	Evaluation []Token
+	Result     string
+}
+
+func (expression *Evaluation) ToString() string {
+	var sb strings.Builder
+	for _, token := range expression.Evaluation {
+		sb.WriteString(token.Body)
+	}
+	return sb.String()
 }
--- server/internal/app/server.go
+++ server/internal/app/server.go
@@ -1,15 +1,16 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
 	"github.com/gorilla/mux"
 	"github.com/sava-cska/SPbSU-Calculator/internal/app/model"
 	"github.com/sava-cska/SPbSU-Calculator/internal/app/storage"
 	"github.com/sirupsen/logrus"
-	"io"
-	"math/rand"
-	"net/http"
-	"strconv"
-	"strings"
 )
 
 type Server struct {
@@ -17,6 +18,15 @@ type Server struct {
 	logger  *logrus.Logger
 	router  *mux.Router
 	storage *storage.Storage
+}
+
+type PostEvalResponse struct {
+	Result string
+}
+
+type GetEvalResponse struct {
+	Evaluation []model.Token
+	Result     string
 }
 
 // New Upsert new instance of server
@@ -59,30 +69,96 @@ func (server *Server) configureRouter() {
 func (server *Server) handleHello() http.HandlerFunc {
 	// Here we can add something, that is the reason behind making functor
 	return func(writer http.ResponseWriter, request *http.Request) {
-		server.logger.Debugf("Called URI %s", request.RequestURI)
+		server.logger.Debugf("GET - Called URI %s", request.RequestURI)
 		io.WriteString(writer, "Hello")
 	}
 }
 
+func (server *Server) handleError(writer http.ResponseWriter, code int, reason string, err error) {
+	server.logger.Errorln(reason, err)
+	writer.WriteHeader(code)
+	writer.Write([]byte(reason))
+}
+
+func (server *Server) parseJSON(value any, writer http.ResponseWriter, request *http.Request) error {
+	data, errReq := io.ReadAll(request.Body)
+	if errReq != nil {
+		server.handleError(writer, http.StatusBadRequest, "Can't read request body.", errReq)
+		return errReq
+	}
+
+	if errJSON := json.Unmarshal(data, value); errJSON != nil {
+		server.handleError(writer, http.StatusBadRequest, "Can't convert body json to correct struct.", errJSON)
+		return errJSON
+	}
+
+	return nil
+}
+
 func (server *Server) handleEvaluationsGet() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		server.logger.Debugf("Called URI %s", request.RequestURI)
-		evals, _ := server.storage.Evaluations().List(request.RequestURI)
-		server.logger.Debugf(strings.Join(evals, " "))
-		io.WriteString(writer, strings.Join(evals, " "))
-		// ...
+		server.logger.Debugf("GET - Called URI %s", request.RequestURI)
+		var user model.Evaluation
+		if errJSON := server.parseJSON(interface{}(&user), writer, request); errJSON != nil {
+			return
+		}
+
+		evals, errList := server.storage.Evaluations().List(user.UserUid)
+		if errList != nil {
+			server.handleError(writer, http.StatusInternalServerError, "Can't get list of evaluations.", errList)
+			return
+		}
+
+		getResponse := make([]GetEvalResponse, len(evals))
+		for idx := 0; idx < len(evals); idx++ {
+			getResponse[idx] = GetEvalResponse{Evaluation: evals[idx].Evaluation, Result: evals[idx].Result}
+		}
+
+		respJSON, errRespJSON := json.Marshal(getResponse)
+		if errRespJSON != nil {
+			server.handleError(writer, http.StatusInternalServerError, "Can't create JSON object from data.", errRespJSON)
+			return
+		}
+
+		writer.Write(respJSON)
 	}
 }
 
 func (server *Server) handleEvaluationsPost() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		server.logger.Debugf("Called URI %s", request.RequestURI)
-		server.storage.Evaluations().Upsert(
-			&model.Evaluation{
-				UserId:            request.RequestURI,
-				EncodedEvaluation: request.RequestURI + strconv.FormatInt(rand.Int63(), 10),
-			})
-		// ...
+		server.logger.Debugf("POST - Called URI %s", request.RequestURI)
+
+		var expression model.Evaluation
+		if errJSON := server.parseJSON(interface{}(&expression), writer, request); errJSON != nil {
+			return
+		}
+
+		stringExpr := expression.ToString()
+		formula, errCreateFormula := govaluate.NewEvaluableExpression(stringExpr)
+		if errCreateFormula != nil {
+			server.handleError(writer, http.StatusBadRequest,
+				fmt.Sprintf("Can't create formula from string representation %s.", stringExpr), errCreateFormula)
+			return
+		}
+
+		result, errEvaluate := formula.Evaluate(nil)
+		if errEvaluate != nil {
+			server.handleError(writer, http.StatusBadRequest, fmt.Sprintf("Can't evaluate formula %s.", stringExpr), errEvaluate)
+			return
+		}
+
+		expression.Result = fmt.Sprintf("%v", result)
+		server.logger.Debugf("Result of the expression %s equals %s", stringExpr, expression.Result)
+
+		server.storage.Evaluations().Upsert(&expression)
+		postResponse := PostEvalResponse{Result: expression.Result}
+		respJSON, errRespJSON := json.Marshal(postResponse)
+		if errRespJSON != nil {
+			server.handleError(writer, http.StatusInternalServerError, "Can't create JSON object from data.", errRespJSON)
+			return
+		}
+
+		writer.Write(respJSON)
 	}
 }
 
