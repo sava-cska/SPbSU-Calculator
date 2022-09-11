83c6c27896538666e625bcf4f346c6115d2f7407
Saveliy Grigoryev <sava-cska@mail.ru>
2022-09-11 12:20:49 +0300

    Add handlers of get/post queries

--- server/go.mod
+++ server/go.mod
@@ -4,14 +4,15 @@ go 1.19
 
 require (
 	github.com/BurntSushi/toml v1.2.0
+	github.com/Knetic/govaluate v3.0.0+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.6
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/lib/pq v1.10.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- server/internal/app/storage/evaluations_dao.go
+++ server/internal/app/storage/evaluations_dao.go
@@ -13,18 +13,18 @@ var tmp = make(map[string][]model.Evaluation)
 // Upsert
 // Add new instance of evaluation, do not throw error if table contains
 func (dao *EvaluationsDAO) Upsert(evaluation *model.Evaluation) (*model.Evaluation, error) {
-	val, exists := tmp[evaluation.UserId]
+	val, exists := tmp[evaluation.UserUid]
 	if !exists {
-		tmp[evaluation.UserId] = make([]model.Evaluation, 0, 5)
+		tmp[evaluation.UserUid] = make([]model.Evaluation, 0, 5)
 	}
-	tmp[evaluation.UserId] = append(val, *evaluation)
+	tmp[evaluation.UserUid] = append(val, *evaluation)
 	return evaluation, nil
 }
 
-func (dao *EvaluationsDAO) List(userId string) ([]string, error) {
-	mapped := make([]string, len(tmp[userId]))
-	for _, v := range tmp[userId] {
-		mapped = append(mapped, v.EncodedEvaluation)
+func (dao *EvaluationsDAO) List(userId string) ([]model.Evaluation, error) {
+	if val, ok := tmp[userId]; ok {
+		return val, nil
+	} else {
+		return []model.Evaluation{}, nil
 	}
-	return mapped, nil
 }
