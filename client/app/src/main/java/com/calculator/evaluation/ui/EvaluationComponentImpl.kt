75864263fc3ea3ce1bbc6d847865d446dc4e7010
Kirill Gavrilov <k.gavrilov@vk.team>
2022-09-12 14:32:42 +0300

    comma fix

--- client/app/src/main/java/com/calculator/evaluation/ui/EvaluationComponentImpl.kt
+++ client/app/src/main/java/com/calculator/evaluation/ui/EvaluationComponentImpl.kt
@@ -153,17 +153,18 @@ class EvaluationComponentImpl(
             } else {
                 newList.add(index, Numeric(value = "0.", Any()))
             }
-            if (index + 1 in newList.indices) {
+            val updatedIndex = newList.indexOf(EmptyField)
+            if (updatedIndex + 1 in newList.indices) {
                 /**
                  * Если справа стоит число, от него точку следует убрать
                  */
-                val next = newList[index + 1]
+                val next = newList[updatedIndex + 1]
                 if (next is Numeric) {
                     val updatedNextValue = next.value.filter { it != '.' }
                     if (updatedNextValue.isEmpty()) {
-                        newList.removeAt(index + 1)
+                        newList.removeAt(updatedIndex + 1)
                     } else {
-                        newList[index + 1] = Numeric(updatedNextValue, Any())
+                        newList[updatedIndex + 1] = Numeric(updatedNextValue, Any())
                     }
                 }
             }
